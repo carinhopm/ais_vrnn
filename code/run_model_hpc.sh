1965adc5f7daf3f560bf86db456d2c5b713564d5
Carlos Parra Marcelo <carlospm211094@hotmail.com>
2021-05-19 10:04:46 +0200

    Some cleaning & experiments files

--- code/run_model_hpc.sh
+++ code/run_model_hpc.sh
@@ -3,19 +3,19 @@
 ### –- specify queue --
 #BSUB -q gpuk80
 ### -- set the job Name --
-#BSUB -J CargTank_1912
-### -- ask for number of cores (default: 1) --
-#BSUB -n 1
+#BSUB -J CargTank_1911
+### -- ask for number of cores (default: 2) --
+#BSUB -n 2
 ### -- specify that the cores should be in the same host --
 #BSUB -R "span[hosts=1]"
 ### -- Select the resources: 2 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 24:00
-# request 16GB of system-memory per core
-#BSUB -R "rusage[mem=16GB]"
+# request 32GB of system-memory per core
+#BSUB -R "rusage[mem=32GB]"
 ### -- specify that we want the job to get killed if it exceeds 8GB per core/slot --
-#BSUB -M 16GB
+#BSUB -M 32GB
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -26,8 +26,8 @@
 #BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -o logs/CargTank_1912_%J.out
-#BSUB -e logs/CargTank_1912_%J.err
+#BSUB -o logs/CargTank_1911_%J.out
+#BSUB -e logs/CargTank_1911_%J.err
 # -- end of LSF options --
 
 
@@ -47,11 +47,6 @@ module load cudnn/v7.6.5.32-prod-cuda-10.2
 # activate the virtual environment which includes the necessary python packages
 source ./python_env/bin/activate
 
-echo "Setup completed. Running script...\n"
+echo "Setup completed. Running training script...\n"
 
-#python3 checking.py
-#python3 run_model.py
-fil-profile run run_model.py
-
-
-
+python3 train_model.py
